af936840c273f8062db3407a9692efe8ebb05844
martinss27 <leoalmeidamartins@gmail.com>
2025-05-19 16:17:12 -0300

    fixing project structure

--- jobtracker_all/manage.py
+++ jobtracker_all/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobtracker_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
